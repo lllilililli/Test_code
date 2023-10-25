72e44f322dc05a5295e3a588a624d27775bb4e42
Jae Seong <130049769+lllilililli@users.noreply.github.com>
2023-10-25 16:34:03 +0900

    Delete WordCloud/Text Extractor/Text_Extractor_1.0.0/install_module.py

--- WordCloud/Text Extractor/Text_Extractor_1.0.0/install_module.py
+++
@@ -1,7 +0,0 @@
-def install():
-    import subprocess
-    subprocess.run(["pip", "install", "wordcloud"]) # pip install wordcloud
-    subprocess.run(["pip", "install", "matplotlib"]) # pip install matplotlib
-    subprocess.run(["pip", "install", "PyPDF2"]) # pip install PyPDF2
-
-install()
